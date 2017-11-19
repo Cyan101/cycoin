07e8339f90686495f64e7bf6d160354b80245ae8
Cyan/Jos <crazyandroid101@gmail.com>
2017-11-19 22:49:37 +0800

    Added template for part 1 + Gemfile

---
+++ part1/cycoin.rb
@@ -0,0 +1,17 @@
+require 'sinatra'
+require 'colorize'
+
+
+# @param user
+get '/balance' do
+end
+
+# @param name
+post '/users' do
+end
+
+# @param from
+# @param to
+# @param amount
+post '/transfers' do
+end
