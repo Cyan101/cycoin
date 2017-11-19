e7c83c4d15e903bfdece4cf4f48ca47ba756bb4d
Cyan/Jos <crazyandroid101@gmail.com>
2017-11-19 23:11:45 +0800

    Added base functionality and client for running tests

--- part1/cycoin.rb
+++ part1/cycoin.rb
@@ -1,17 +1,34 @@
 require 'sinatra'
 require 'colorize'
 
+BALANCES = {
+  'cyan' => 1_000,
+}
 
 # @param user
 get '/balance' do
+  user = params['user']
+  puts BALANCES.to_s.yellow
+  "#{user} has #{BALANCES[user]}"
 end
 
 # @param name
 post '/users' do
+  name = params['name']
+  BALANCES[name] ||= 0
+  puts BALANCES.to_s.yellow
+  "Done~!"
 end
 
 # @param from
 # @param to
 # @param amount
 post '/transfers' do
+  from, to = params.values_at('from', 'to').map(&:downcase)
+  amount = params['amount'].to_i
+  raise unless BALANCES[from] >= amount
+  BALANCES[from] -= amount
+  BALANCES[to] += amount
+  puts BALANCES.to_s.yellow
+  "Done!"
 end
